af88a24271d0aef62f1eecfbcc3d7c6ff56777ae
alekseytemerbek <tech.temerbek@gmail.com>
2024-02-16 21:23:13 +0300

    add date notes selecting

--- notes.py
+++ notes.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import datetime
 import json
 import os
 import lorem
@@ -35,6 +36,7 @@ def print_note_to_file(filename: str, data: dict):
 def print_all_notes_to_file(filename: str, data: dict):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
+
 
 def read_note_from_file(filename: str):
     with open(filename, 'r', encoding='utf-8') as f:
@@ -84,20 +86,36 @@ def delete_note(filename: str, index: str):
     print_all_notes_to_file(filename, ds)
     print(f'Запись №{index} успешно удалена ')
 
+
 # Support method
 def create_random_notes(quantity: int):
     filename = 'test.json'
     ds = dict()
     for i in range(0, quantity):
-        ds['name'] = 'note_' + str(i)
-        ds['body'] = lorem.lorem
-        ds['time_create'] = datetime.datetime.now().isoformat()
-        ds['time_changed'] = datetime.datetime.now().isoformat()
+        ds[name] = 'note_' + str(i)
+        ds[body] = lorem.lorem
+        ds[time_create] = datetime.datetime.now().isoformat()
+        ds[time_changed] = datetime.datetime.now().isoformat()
         print_note_to_file(filename, ds)
+
+def date_notes_select(filename: str, start: str, end: str):
+    start_point = datetime.strptime(start, "%Y-%m-%d")
+    end_point = datetime.strptime(end, "%Y-%m-%d")
+    if count_notes(filename) == 0:
+        print('База заметок пуста или не существует')
+        return
+    data = read_note_from_file(filename)
+    for key in data.keys():
+        time_point = datetime.strptime(data[key][time_changed][:9:], "%Y-%m-%d")
+        if start_point < time_point < end_point:
+            print(
+                f'Идентификатор: {key}, Имя записи: {data[key][name]}, '
+                f'Дата последнего изменения: {data[key][time_changed]}')
 
 
 # create_random_notes(10)
-show_all_notes('test.json')
+# show_all_notes('test.json')
+date_notes_select('test.json', '2024-01-01', '2025-01-01')
 # change_note('test.json', '2')
-delete_note('test.json', '2')
+# delete_note('test.json', '2')
 
