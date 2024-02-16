442f52de9cc66403ddc2b0e2f66a86d02e54f7f9
alekseytemerbek <tech.temerbek@gmail.com>
2024-02-16 18:25:47 +0300

    add change note method

--- notes.py
+++ notes.py
@@ -4,16 +4,20 @@ import os
 import lorem
 
 notesfile = 'notes.json'
+name = 'name'
+body = 'body'
+time_create = 'time_create'
+time_changed = 'time_changed'
 
 
 def create_notes():
     note_name = input('Input note name: ')
     note_body = input('Input your notes: ')
     note = dict()
-    note['name'] = note_name
-    note['body'] = note_body
-    note['time_create'] = datetime.datetime.now().isoformat()
-    note['time_changed'] = datetime.datetime.now().isoformat()
+    note[name] = note_name
+    note[body] = note_body
+    note[time_create] = datetime.datetime.now().isoformat()
+    note[time_changed] = datetime.datetime.now().isoformat()
     return note
 
 
@@ -26,8 +30,11 @@ def print_note_to_file(filename: str, data: dict):
         notes[1] = data
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(notes, file, ensure_ascii=False, indent=4)
-    print()
 
+
+def print_all_notes_to_file(filename: str, data: dict):
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
 def read_note_from_file(filename: str):
     with open(filename, 'r', encoding='utf-8') as f:
@@ -52,9 +59,20 @@ def show_all_notes(filename: str):
         return
     data = read_note_from_file(filename)
     for key in data.keys():
-        name = 'name'
-        time = 'time_changed'
-        print(f'Идентификатор: {key}, Имя записи: {data[key][name]}, Дата последнего изменения: {data[key][time]}')
+        print(
+            f'Идентификатор: {key}, Имя записи: {data[key][name]}, Дата последнего изменения: {data[key][time_changed]}')
+
+
+def change_note(filename: str, index: str):
+    if count_notes(filename) == 0:
+        print('База заметок пуста или не существует')
+        return
+    data = read_note_from_file(filename)
+    data[index][body] = input('Введите новый текст заметки: ')
+    data[index][time_changed] = datetime.datetime.now().isoformat()
+
+    print_all_notes_to_file(filename, data)
+    print(f'Запись № {index}  {data[index][name]} была успешно изменена в {data[index][time_changed]}')
 
 
 # Support method
@@ -69,4 +87,6 @@ def create_random_notes(quantity: int):
         print_note_to_file(filename, ds)
 
 
+# create_random_notes(10)
 show_all_notes('test.json')
+change_note('test.json', '2')
